740b268eecce79315090902761e86f1b4038242a
Ivan Nefedov <Tyuiu.NefedovIS@outlook.com>
2024-12-19 17:25:17 +0500

    Дополнение .csv файла, поиск и отобр. информации по книге ч/з артикул

--- Project.V4/FormMain.Designer.cs
+++ Project.V4/FormMain.Designer.cs
@@ -28,22 +28,31 @@
         /// </summary>
         private void InitializeComponent()
         {
-            panelButtons_NIS = new Panel();
-            buttonRefreshCSV_NIS = new Button();
             dataGridView_NIS = new DataGridView();
-            panelButtons_NIS.SuspendLayout();
+            buttonRefreshCSV_NIS = new Button();
+            panelButtons_NIS = new Panel();
+            labelArticul_NIS = new Label();
+            textBoxArticul_NIS = new TextBox();
+            buttonFindBook_NIS = new Button();
+            tableLayoutPanel_NIS = new TableLayoutPanel();
             ((System.ComponentModel.ISupportInitialize)dataGridView_NIS).BeginInit();
+            panelButtons_NIS.SuspendLayout();
             SuspendLayout();
             // 
-            // panelButtons_NIS
+            // dataGridView_NIS
             // 
-            panelButtons_NIS.BorderStyle = BorderStyle.Fixed3D;
-            panelButtons_NIS.Controls.Add(buttonRefreshCSV_NIS);
-            panelButtons_NIS.Dock = DockStyle.Top;
-            panelButtons_NIS.Location = new Point(0, 0);
-            panelButtons_NIS.Name = "panelButtons_NIS";
-            panelButtons_NIS.Size = new Size(1008, 68);
-            panelButtons_NIS.TabIndex = 1;
+            dataGridView_NIS.AllowUserToAddRows = false;
+            dataGridView_NIS.AllowUserToDeleteRows = false;
+            dataGridView_NIS.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            dataGridView_NIS.AutoSizeRowsMode = DataGridViewAutoSizeRowsMode.AllCells;
+            dataGridView_NIS.BackgroundColor = SystemColors.Control;
+            dataGridView_NIS.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView_NIS.Dock = DockStyle.Left;
+            dataGridView_NIS.Location = new Point(0, 68);
+            dataGridView_NIS.Name = "dataGridView_NIS";
+            dataGridView_NIS.ReadOnly = true;
+            dataGridView_NIS.Size = new Size(810, 541);
+            dataGridView_NIS.TabIndex = 2;
             // 
             // buttonRefreshCSV_NIS
             // 
@@ -57,40 +66,90 @@
             buttonRefreshCSV_NIS.UseVisualStyleBackColor = true;
             buttonRefreshCSV_NIS.Click += buttonRefreshCSV_NIS_Click;
             // 
-            // dataGridView_NIS
+            // panelButtons_NIS
             // 
-            dataGridView_NIS.AllowUserToAddRows = false;
-            dataGridView_NIS.AllowUserToDeleteRows = false;
-            dataGridView_NIS.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
-            dataGridView_NIS.AutoSizeRowsMode = DataGridViewAutoSizeRowsMode.AllCells;
-            dataGridView_NIS.BackgroundColor = SystemColors.Control;
-            dataGridView_NIS.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dataGridView_NIS.Dock = DockStyle.Left;
-            dataGridView_NIS.Location = new Point(0, 68);
-            dataGridView_NIS.Name = "dataGridView_NIS";
-            dataGridView_NIS.Size = new Size(810, 505);
-            dataGridView_NIS.TabIndex = 2;
+            panelButtons_NIS.BorderStyle = BorderStyle.Fixed3D;
+            panelButtons_NIS.Controls.Add(labelArticul_NIS);
+            panelButtons_NIS.Controls.Add(textBoxArticul_NIS);
+            panelButtons_NIS.Controls.Add(buttonFindBook_NIS);
+            panelButtons_NIS.Controls.Add(buttonRefreshCSV_NIS);
+            panelButtons_NIS.Dock = DockStyle.Top;
+            panelButtons_NIS.Location = new Point(0, 0);
+            panelButtons_NIS.Name = "panelButtons_NIS";
+            panelButtons_NIS.Size = new Size(1232, 68);
+            panelButtons_NIS.TabIndex = 1;
+            // 
+            // labelArticul_NIS
+            // 
+            labelArticul_NIS.AutoSize = true;
+            labelArticul_NIS.Location = new Point(168, 13);
+            labelArticul_NIS.Name = "labelArticul_NIS";
+            labelArticul_NIS.Size = new Size(86, 15);
+            labelArticul_NIS.TabIndex = 3;
+            labelArticul_NIS.Text = "Ввод артикула";
+            // 
+            // textBoxArticul_NIS
+            // 
+            textBoxArticul_NIS.Location = new Point(162, 30);
+            textBoxArticul_NIS.Name = "textBoxArticul_NIS";
+            textBoxArticul_NIS.Size = new Size(100, 23);
+            textBoxArticul_NIS.TabIndex = 2;
+            // 
+            // buttonFindBook_NIS
+            // 
+            buttonFindBook_NIS.AutoSize = true;
+            buttonFindBook_NIS.Dock = DockStyle.Left;
+            buttonFindBook_NIS.Location = new Point(75, 0);
+            buttonFindBook_NIS.Name = "buttonFindBook_NIS";
+            buttonFindBook_NIS.Size = new Size(85, 64);
+            buttonFindBook_NIS.TabIndex = 1;
+            buttonFindBook_NIS.Text = "Найти книгу";
+            buttonFindBook_NIS.UseVisualStyleBackColor = true;
+            buttonFindBook_NIS.Click += button1_Click;
+            // 
+            // tableLayoutPanel_NIS
+            // 
+            tableLayoutPanel_NIS.ColumnCount = 1;
+            tableLayoutPanel_NIS.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
+            tableLayoutPanel_NIS.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
+            tableLayoutPanel_NIS.Dock = DockStyle.Top;
+            tableLayoutPanel_NIS.Location = new Point(810, 68);
+            tableLayoutPanel_NIS.Name = "tableLayoutPanel_NIS";
+            tableLayoutPanel_NIS.RowCount = 6;
+            tableLayoutPanel_NIS.RowStyles.Add(new RowStyle(SizeType.Percent, 50.8333321F));
+            tableLayoutPanel_NIS.RowStyles.Add(new RowStyle(SizeType.Percent, 49.1666679F));
+            tableLayoutPanel_NIS.RowStyles.Add(new RowStyle(SizeType.Absolute, 27F));
+            tableLayoutPanel_NIS.RowStyles.Add(new RowStyle(SizeType.Absolute, 28F));
+            tableLayoutPanel_NIS.RowStyles.Add(new RowStyle(SizeType.Absolute, 28F));
+            tableLayoutPanel_NIS.RowStyles.Add(new RowStyle(SizeType.Absolute, 299F));
+            tableLayoutPanel_NIS.Size = new Size(422, 503);
+            tableLayoutPanel_NIS.TabIndex = 3;
             // 
             // FormMain
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1008, 573);
+            ClientSize = new Size(1232, 609);
+            Controls.Add(tableLayoutPanel_NIS);
             Controls.Add(dataGridView_NIS);
             Controls.Add(panelButtons_NIS);
-            MinimumSize = new Size(1024, 608);
+            MinimumSize = new Size(1128, 612);
             Name = "FormMain";
-            Text = "Form1";
+            Text = "Я РАЗЪЕБУ ВЕСЬ МИР И РАЗЪЕБУСЬ ВМЕСТЕ С НИМ";
             Load += FormMain_Load;
+            ((System.ComponentModel.ISupportInitialize)dataGridView_NIS).EndInit();
             panelButtons_NIS.ResumeLayout(false);
             panelButtons_NIS.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)dataGridView_NIS).EndInit();
             ResumeLayout(false);
         }
 
         #endregion
-        private Panel panelButtons_NIS;
-        private Button buttonRefreshCSV_NIS;
         private DataGridView dataGridView_NIS;
+        private Button buttonRefreshCSV_NIS;
+        private Panel panelButtons_NIS;
+        private Button buttonFindBook_NIS;
+        private Label labelArticul_NIS;
+        private TextBox textBoxArticul_NIS;
+        private TableLayoutPanel tableLayoutPanel_NIS;
     }
 }
--- Project.V4/FormMain.cs
+++ Project.V4/FormMain.cs
@@ -5,6 +5,8 @@ namespace Project.V4
     public partial class FormMain : Form
     {
         DataService ds = new DataService();
+        public string path;
+        public string[,] matrix;
         public FormMain()
         {
             InitializeComponent();
@@ -12,16 +14,16 @@ namespace Project.V4
 
         private void FormMain_Load(object sender, EventArgs e)
         {
-
+            
         }
 
         private void buttonRefreshCSV_NIS_Click(object sender, EventArgs e)
         {
-            string path = @"C:\Users\cumsh\source\repos\Tyuiu.NefedovIS.Sprint7\Books.csv";
-            string[,] matrix = ds.CsvToMatrix(path);
             dataGridView_NIS.Rows.Clear();
             dataGridView_NIS.Columns.Clear();
-            for (int col = 0; col < matrix.GetLength(1); col++)
+            string path = @"C:\Users\cumsh\source\repos\Tyuiu.NefedovIS.Sprint7\Books.csv";
+            string[,] matrix = ds.CsvToMatrix(path);
+            for (int col = 0; col <= 5; col++)
             {
                 dataGridView_NIS.Columns.Add($"Column{col}", matrix[0, col]);
             }
@@ -42,6 +44,125 @@ namespace Project.V4
                     rowData[col] = matrix[row, col];
                 }
                 dataGridView_NIS.Rows.Add(rowData);
+            }
+        }
+
+        private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string path = @"C:\Users\cumsh\source\repos\Tyuiu.NefedovIS.Sprint7\Books.csv";
+            string[,] matrix = ds.CsvToMatrix(path);
+            string input = textBoxArticul_NIS.Text;
+            //Проверка на введение артикула
+            if (string.IsNullOrWhiteSpace(input))
+            {
+                MessageBox.Show("Пожалуйста, введите артикул книги", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+            foreach (char c in input)
+            {
+                if (!char.IsDigit(c))
+                {
+                    MessageBox.Show("Пожалуйста, проверьте правильность ввода.\nАртикул содержит только целочисленные значения.", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    return;
+                }
+            }
+            //Поиск названия книги по артикулу
+            bool BookFound = false;
+            for (int row = 1; row < matrix.GetLength(0); row++)
+            {
+                if (matrix[row, 5] == input)
+                {
+                    //Определение переменных для инфо о книгах
+                    string author = matrix[row, 0];
+                    string bookname = matrix[row, 1];
+                    string year = matrix[row, 2];
+                    string publisher = matrix[row, 3];
+                    string price = matrix[row, 4];
+                    string article = matrix[row, 5];
+                    string description = matrix[row, 6];
+
+                    tableLayoutPanel_NIS.Controls.Clear();
+                    BookFound = true;
+                    //Жёстко создаём текст
+                    Label BookName = new Label
+                    {
+                        Text = bookname,
+                        AutoSize = true,
+                        Font = new Font("Segoe UI", 12, FontStyle.Bold),
+                        TextAlign = ContentAlignment.MiddleCenter,
+                        Anchor = AnchorStyles.None
+                    };
+                    tableLayoutPanel_NIS.Controls.Add(BookName, 0, 0);
+
+                    Label BookAuthor = new Label
+                    {
+                        Text = author,
+                        AutoSize = true,
+                        Font = new Font("Segoe UI", 10, FontStyle.Bold),
+                        TextAlign = ContentAlignment.MiddleCenter,
+                        Anchor = AnchorStyles.None
+                    };
+                    tableLayoutPanel_NIS.Controls.Add(BookAuthor, 1, 0);
+                    
+                    Label BookYear = new Label
+                    {
+                        Text = year,
+                        AutoSize = true,
+                        Font = new Font("Segoe UI", 9),
+                        TextAlign = ContentAlignment.MiddleCenter,
+                        Anchor = AnchorStyles.None
+                    };
+                    tableLayoutPanel_NIS.Controls.Add(BookYear, 2, 0);
+
+                    Label BookPublisher = new Label
+                    {
+                        Text = publisher,
+                        AutoSize = true,
+                        Font = new Font("Segoe UI", 9),
+                        TextAlign = ContentAlignment.MiddleCenter,
+                        Anchor = AnchorStyles.None
+                    };
+                    tableLayoutPanel_NIS.Controls.Add(BookPublisher, 3, 0);
+
+                    Label BookPrice = new Label
+                    {
+                        Text = price + " руб.",
+                        AutoSize = true,
+                        Font = new Font("Segoe UI", 9),
+                        TextAlign = ContentAlignment.MiddleCenter,
+                        Anchor = AnchorStyles.None
+                    };
+                    tableLayoutPanel_NIS.Controls.Add(BookPrice, 4, 0);
+
+                    Label BookDescription = new Label
+                    {
+                        Text = description,
+                        AutoSize = true,
+                        Font = new Font("Segoe UI", 9),
+                        TextAlign = ContentAlignment.MiddleCenter,
+                        Anchor = AnchorStyles.None
+                    };
+                    tableLayoutPanel_NIS.Controls.Add(BookDescription, 5, 0);
+
+                    // Очистка TextBox после добавления
+                    textBoxArticul_NIS.Clear();
+                    textBoxArticul_NIS.Focus();
+                    return;
+                }
+            }
+            if (!BookFound)
+            {
+                MessageBox.Show("Книга не найдена. Пожалуйста, проверьте соответсвие введёного артикула.", "Книга не найдена", MessageBoxButtons.OK, MessageBoxIcon.Warning);
             }
         }
     }
